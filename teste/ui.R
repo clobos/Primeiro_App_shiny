cee9462357a4234dd7316155fe812fe7c6cc7ac6
Cristian Villegas Lobos <master.villegas@gmail.com>
2015-12-12 19:49:02 -0200

    Update ui.R

--- teste/ui.R
+++ teste/ui.R
@@ -1,1 +1,29 @@
+library(shiny)
+fluidPage(
+  headerPanel("Uso do Shiny para o ensino da Estatística"),
+  sidebarPanel(
+    selectInput("distribuicao", 
+                "Escolher uma distribuição", 
+                choices = c("Normal", "Exponencial")),
+    
+    selectInput(inputId = "Cor", 
+                label=span("Escolha cor histograma", 
+                style = "color:darkred"), 
+                choices = colors(),
+                selected = "yellow"),
+    
+    sliderInput("tamanho", 
+                "Tamanho amostral...",
+                min=1000,max=5000, value=500,step=50),
+    
+    conditionalPanel("input.distribuicao == 'Normal' ",
+    textInput("Media", "Escolher media", 10),
+    textInput("Desvio", "Escolher DP", 3)),
+    
+    conditionalPanel("input.distribuicao == 'Exponencial' ",
+    textInput("lambda", "Escolher Taxa", 2))
+    
+  ),
+  mainPanel( plotOutput("grafico") )
+)
 
